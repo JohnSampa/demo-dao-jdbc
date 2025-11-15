af757e65b941ca031f8777ed4428f181d5dc39da
Jonathan Sampaio <120474111+JohnSampa@users.noreply.github.com>
2025-11-15 18:06:20 -0300

    SellerDaoJDBC implements methods findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,17 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department dpt = new Department(1,"Books");
-        System.out.println(dpt);
-
-        Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),3000.00,dpt);
-        System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(1);
+
+        System.out.println(seller);
 
     }
 }
