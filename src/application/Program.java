f14bfe48de749773003ccea1fbcecc5fdacef570
Jonathan Sampaio <120474111+JohnSampa@users.noreply.github.com>
2025-11-15 13:38:36 -0300

    add class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dpt = new Department(1,"Books");
         System.out.println(dpt);
+
+        Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),3000.00,dpt);
+        System.out.println(seller);
     }
 }
